5ce15399f06487fd39344433ef25727fd3d8418d
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2023-03-11 02:50:28 +0900

    私は最強

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+export const App: React.FC = () => {
+  const [count, setCount] = useState(1);
+  return (
+    <Flex draggable>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </Button>
+      <Text>現在{count}戦目</Text>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </Button>
+    </Flex>
+  );
+};
+
+const Flex = styled(Box)`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Text = styled(Typography)`
+  font-size: 4rem;
+`;
